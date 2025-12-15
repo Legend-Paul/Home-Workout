89e74857907d7e84b0f32e7d5cb5f31ae10c3faa
Legend-Paul <qamunyamaina950@gmail.com>
2025-12-15 11:05:54 +0300

    Set up the server

---
+++ backend/src/app.ts
@@ -0,0 +1,26 @@
+import express, {
+  type Request,
+  type Response,
+  type NextFunction,
+} from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// error handling middleware
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(error.stack);
+  res.status(500).send("Something broke!");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
