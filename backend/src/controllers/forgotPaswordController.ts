467fe8cdb3f588c7309650621f2a3be0bdeb9306
Legend-Paul <qamunyamaina950@gmail.com>
2025-12-16 22:24:19 +0300

    Add forgot password handler

--- backend/src/controllers/forgotPaswordController.ts
+++ backend/src/controllers/forgotPaswordController.ts
@@ -1,5 +1,60 @@
-import type {Request, Response} from "express";
+import type { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import { prisma } from "../lib/prisma.js";
+import { body, validationResult } from "express-validator";
 
-const updatePassword =(req: Request, res: Response)=>{}
+const validate = [
+  body("email").trim().isEmail().withMessage("Invalid email format"),
+  body("password")
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long"),
+  body("confirmPassword")
+    .trim()
+    .isLength({ min: 8 })
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage("Passwords do not match"),
+];
 
-export {updatePassword}
+interface Data {
+  email: string;
+  password: string;
+}
+
+const updatePasswordHandler = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+    return;
+  }
+
+  const { email, password }: Data = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      res.status(400).json({ message: "User not found" });
+      return;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.user.update({
+      where: { email },
+      data: { password: hashedPassword },
+    });
+
+    res.status(200).json({ message: "Password updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+const updatePassword = [...validate, updatePasswordHandler];
+
+export { updatePassword };
