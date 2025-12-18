5e99bc2591dc14d7fefe94ffc136e70bb1de860c
Legend-Paul <qamunyamaina950@gmail.com>
2025-12-18 12:15:13 +0300

    Create workout exercise model

--- backend/src/controllers/exerciseController.ts
+++ backend/src/controllers/exerciseController.ts
@@ -1,5 +1,11 @@
 import type { Request, Response } from "express";
+import { prisma } from "../lib/prisma.js";
+import { body, validationResult } from "express-validator";
 
+const createExerciseHandler = async (
+  req: Request,
+  res: Response
+): Promise<void> => {};
 export const createExercise = async (req: Request, res: Response) => {};
 
 export const getAllExercises = async (req: Request, res: Response) => {};
