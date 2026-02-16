9874ef7f7a5b18b3970acac715ce367b328ae44f
Legend-Paul <qamunyamaina950@gmail.com>
2026-02-16 12:37:59 +0300

    Create router file for SPA

---
+++ frontend/src/router.ts
@@ -0,0 +1,32 @@
+type RouterHandler = () => void;
+const routes = new Map<string, RouterHandler>();
+
+let notFoundHandler: RouterHandler | null = null;
+
+export function register(path: string, handler: RouterHandler) {
+  routes.set(path, handler);
+}
+
+export function notFound(handler: RouterHandler) {
+  notFoundHandler = handler;
+}
+
+export function handleRoute() {
+  const path = window.location.pathname;
+  const handler = routes.get(path);
+  if (handler) {
+    handler();
+  } else if (notFoundHandler) {
+    notFoundHandler();
+  }
+}
+
+export function navigate(path: string) {
+  window.history.pushState({}, "", path);
+  handleRoute();
+}
+
+export function statrtRouter() {
+  window.addEventListener("popstate", () => handleRoute());
+  handleRoute();
+}
