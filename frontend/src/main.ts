0b41c28bc7dcca1c85f3f85b2759f34120efafc4
Legend-Paul <qamunyamaina950@gmail.com>
2026-02-16 14:13:31 +0300

    Add css variables

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import Home from "./pages/Home/home";
 import { register, startRouter, notFound } from "./router";
 import ErrorRoute from "./errorRoute/errorRoute";
+import "./style.css";
 
 register("/", Home);
 
