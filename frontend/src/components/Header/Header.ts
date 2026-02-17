4a558eaf41dcec05cbc1da43ebfd7f3d2f1d7acd
Legend-Paul <qamunyamaina950@gmail.com>
2026-02-17 11:13:52 +0300

    Change user profile with sign in and signout icons

--- frontend/src/components/Header/Header.ts
+++ frontend/src/components/Header/Header.ts
@@ -27,9 +27,19 @@ export default function Header() {
 
     <div class="${styles["header-icon-container"]}">
       <div class="${styles["user-profile"]}">
-          <svg class="nav-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
-          </svg>
+          <span class="${styles["signin-icon"]}">
+              <svg class="nav-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/>
+              </svg>
+              <p>Sign In</p>
+          </span>
+          <span class="${styles["signout-icon"]}">
+              <svg class="nav-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
+              </svg>
+              <p>Sign Out</p>
+          </span>
+          
           <div class="${styles["user-dropdown"]}">
               <span>Profile</span>
               <span>Settings</span>
