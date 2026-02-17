0970108645c90b139a338f2e9f2819d64ed0da24
Legend-Paul <qamunyamaina950@gmail.com>
2026-02-17 12:20:35 +0300

    Change signin and signout to link

--- frontend/src/errorRoute/errorRoute.ts
+++ frontend/src/errorRoute/errorRoute.ts
@@ -7,7 +7,7 @@ export default function ErrorRoute() {
   <main class="${styles["error-container"]}">
     <h1>404 Not Found</h1>
     <p>The page you are looking for does not exist.</p>
-    <a href="#" id="${styles["home-link"]}">Go back to Home</a>
+    <a href="/" id="${styles["home-link"]}">Go back to Home</a>
   </main>
     `;
 
