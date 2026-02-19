7053b47d15604aa5b0dd05aac7359469676b8f1c
Legend-Paul <qamunyamaina950@gmail.com>
2026-02-19 17:39:30 +0300

    Remove verification email route and move it to sign up route

--- backend/src/routes/verificationEmail.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { verifyVerificationEmail } from "../controllers/verificationEmail.js";
-
-const verificationEmailRouter = Router();
-verificationEmailRouter.post("/signup/:token", verifyVerificationEmail);
-export default verificationEmailRouter;
