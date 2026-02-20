f0fa0c81b4669a36d6006e29795efe9a0be9cc35
Legend-Paul <qamunyamaina950@gmail.com>
2026-02-21 00:16:10 +0300

    Create exercise page aside to filter exercises

--- frontend/src/pages/Exercises/Exercises.ts
+++ frontend/src/pages/Exercises/Exercises.ts
@@ -6,24 +6,27 @@ export default async function Exercises() {
   const mainApp = document.getElementById("main-app");
   mainApp!.innerHTML = `
    <div class="${styles["exercises-container"]}">
-        <div class="${styles["exercise-filter"]}">
-            <h2>Filter Exercises</h2>
-            <div class="${styles["filter-group"]}">
-                <div class="${styles["filter-group"]}">
-                <form id="search-form">
+        <div class="${styles["exercise-filter"]}">             
+                <form id="${styles["search-form"]}">
                     ${Input({
                       id: "search",
                       name: "search",
                       placeholder: "Search exercises...",
-                      label: "Search:",
+                      label: "",
                     })}
                     ${Button({
-                      label: "Search",
+                      label: `<svg class="nav-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
+</svg>`,
                       type: "submit",
                     })}
-            </div>
+                    
+                </form>
+          
+            <h2>Filter Exercises</h2>            
+            <div class="${styles["filter-group"]}">
                 <label for="muscle-group">Muscle Group:</label>
-                <select id="muscle-group">
+                <select id="${styles["muscle-group"]}">
                     <option value="">All</option>
                     <option value="chest">Chest</option>
                     <option value="back">Back</option>
@@ -34,7 +37,7 @@ export default async function Exercises() {
             </div>
             <div class="${styles["filter-group"]}">
                 <label for="equipment">Equipment:</label>
-                <select id="equipment">
+                <select id="${styles["equipment"]}">
                     <option value="">All</option>
                     <option value="dumbbell">Dumbbell</option>
                     <option value="barbell">Barbell</option>
@@ -44,7 +47,7 @@ export default async function Exercises() {
             </div>
             <div class="${styles["filter-group"]}">
                 <label for="difficulty">Difficulty:</label>
-                <select id="difficulty">
+                <select id="${styles["difficulty"]}">
                     <option value="">All</option>
                     <option value="beginner">Beginner</option>
                     <option value="intermediate">Intermediate</option>
@@ -53,7 +56,7 @@ export default async function Exercises() {
             </div>
             <div class="${styles["filter-group"]}">
                 <label for="exercise-status">Exercise status:</label>
-                <select id="exercise-status">
+                <select id="${styles["exercise-status"]}">
                     <option value="">All</option>
                     <option value="active">Active</option>
                     <option value="inactive">Inactive</option>
@@ -63,14 +66,54 @@ export default async function Exercises() {
                 ${Button({
                   label: "Apply Filters",
                   type: "button",
+                  btnClass: styles["filter-action-btn"],
                 })}
                 ${Button({
                   label: "Reset Filters",
                   type: "button",
+                  btnClass: styles["filter-action-btn"],
                 })}
             </div>
         </div>
         <div class="${styles["exercise-list"]}">
+                <div class="${styles["exercise-item"]}">
+                    <div class="${styles["exercise-image"]}">
+                        <img src="https://via.placeholder.com/150" alt="Push-up">
+                    </div>
+                    <h3>Push-up</h3>
+                    <p>Muscle Group: Chest</p>
+                    <p>Equipment: Bodyweight</p>
+                    <p>Difficulty: Beginner</p>
+                    <div class="${styles["exercise-acions"]}">
+                        ${Button({
+                          label: "Edit",
+                          type: "button",
+                        })}
+                        ${Button({
+                          label: "Delete",
+                          type: "button",
+                        })}
+                    </div>
+                </div>
+                <div class="${styles["exercise-item"]}">
+                    <div class="${styles["exercise-image"]}">
+                        <img src="https://via.placeholder.com/150" alt="Push-up">
+                    </div>
+                    <h3>Push-up</h3>
+                    <p>Muscle Group: Chest</p>
+                    <p>Equipment: Bodyweight</p>
+                    <p>Difficulty: Beginner</p>
+                    <div class="${styles["exercise-acions"]}">
+                        ${Button({
+                          label: "Edit",
+                          type: "button",
+                        })}
+                        ${Button({
+                          label: "Delete",
+                          type: "button",
+                        })}
+                    </div>
+                </div>
                 <div class="${styles["exercise-item"]}">
                     <div class="${styles["exercise-image"]}">
                         <img src="https://via.placeholder.com/150" alt="Push-up">
