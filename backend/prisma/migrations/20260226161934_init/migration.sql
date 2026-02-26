232987f9d103c3c697ce682399f85ea911cbe274
Legend-Paul <qamunyamaina950@gmail.com>
2026-02-26 19:33:10 +0300

    Add plan table for custom and qick plan users

--- backend/prisma/migrations/20260226161934_init/migration.sql
+++ backend/prisma/migrations/20260226161934_init/migration.sql
@@ -14,9 +14,6 @@ CREATE TYPE "Status" AS ENUM ('PENDING', 'COMPLETED', 'SKIPPED', 'REST');
 CREATE TYPE "SetupMethod" AS ENUM ('QUICK_START', 'CUSTOM');
 
 -- CreateEnum
-CREATE TYPE "Days" AS ENUM ('SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY');
-
--- CreateEnum
 CREATE TYPE "FriendshipStatus" AS ENUM ('PENDING', 'ACCEPTED', 'BLOCKED');
 
 -- CreateTable
@@ -29,12 +26,35 @@ CREATE TABLE "User" (
     "level" "Level",
     "role" "Role" NOT NULL DEFAULT 'USER',
     "isBoarded" BOOLEAN NOT NULL DEFAULT false,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
     "onboardingStep" INTEGER NOT NULL DEFAULT 0,
     "setupMethod" "SetupMethod" NOT NULL DEFAULT 'QUICK_START',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "VerificationToken" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "VerificationToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PasswordResetToken" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "PasswordResetToken_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -61,41 +81,74 @@ CREATE TABLE "QuickStartPlan" (
     "name" TEXT NOT NULL,
     "goal" "Goal" NOT NULL,
     "level" "Level" NOT NULL,
-    "dayOfWeek" INTEGER NOT NULL,
-    "dayName" "Days" NOT NULL,
-    "muscleGroup" TEXT[],
-    "isRestDay" BOOLEAN NOT NULL DEFAULT false,
     "isActive" BOOLEAN NOT NULL DEFAULT true,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "createdBy" TEXT,
 
     CONSTRAINT "QuickStartPlan_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "QuickStartExercise" (
+CREATE TABLE "QuickStartWeeklyPlan" (
     "id" TEXT NOT NULL,
     "quickStartPlanId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "dayOfWeek" INTEGER NOT NULL,
+    "muscleGroup" TEXT[],
+    "isRestDay" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "QuickStartWeeklyPlan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "QuickStartExercise" (
+    "id" TEXT NOT NULL,
+    "quickStartWeeklyPlanId" TEXT NOT NULL,
     "exerciseId" TEXT NOT NULL,
     "order" INTEGER NOT NULL,
     "reps" INTEGER,
     "sets" INTEGER,
     "duration" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "QuickStartExercise_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "WeeklyPlan" (
+CREATE TABLE "QuickPlanUser" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
+    "quickStartPlanId" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "QuickPlanUser_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserPlan" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserPlan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WeeklyPlan" (
+    "id" TEXT NOT NULL,
+    "userPlanId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "dayOfWeek" INTEGER NOT NULL,
-    "dayName" "Days" NOT NULL,
     "muscleGroup" TEXT[],
     "isRestDay" BOOLEAN NOT NULL DEFAULT false,
     "isActive" BOOLEAN NOT NULL DEFAULT true,
-    "quickStartPlanId" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -148,22 +201,37 @@ CREATE TABLE "Friendship" (
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Exercise_name_key" ON "Exercise"("name");
+CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PasswordResetToken_token_key" ON "PasswordResetToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Exercise_name_createdBy_key" ON "Exercise"("name", "createdBy");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "QuickStartPlan_name_key" ON "QuickStartPlan"("name");
 
 -- CreateIndex
-CREATE INDEX "QuickStartExercise_quickStartPlanId_idx" ON "QuickStartExercise"("quickStartPlanId");
+CREATE UNIQUE INDEX "QuickStartWeeklyPlan_quickStartPlanId_dayOfWeek_key" ON "QuickStartWeeklyPlan"("quickStartPlanId", "dayOfWeek");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "QuickStartExercise_quickStartPlanId_exerciseId_key" ON "QuickStartExercise"("quickStartPlanId", "exerciseId");
+CREATE INDEX "QuickStartExercise_quickStartWeeklyPlanId_idx" ON "QuickStartExercise"("quickStartWeeklyPlanId");
 
 -- CreateIndex
-CREATE INDEX "WeeklyPlan_userId_idx" ON "WeeklyPlan"("userId");
+CREATE UNIQUE INDEX "QuickStartExercise_quickStartWeeklyPlanId_exerciseId_key" ON "QuickStartExercise"("quickStartWeeklyPlanId", "exerciseId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "WeeklyPlan_userId_dayOfWeek_key" ON "WeeklyPlan"("userId", "dayOfWeek");
+CREATE UNIQUE INDEX "QuickPlanUser_userId_quickStartPlanId_key" ON "QuickPlanUser"("userId", "quickStartPlanId");
+
+-- CreateIndex
+CREATE INDEX "UserPlan_userId_idx" ON "UserPlan"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserPlan_userId_name_key" ON "UserPlan"("userId", "name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "WeeklyPlan_userPlanId_dayOfWeek_key" ON "WeeklyPlan"("userPlanId", "dayOfWeek");
 
 -- CreateIndex
 CREATE INDEX "WeeklyPlanExercise_weeklyPlanId_idx" ON "WeeklyPlanExercise"("weeklyPlanId");
@@ -175,9 +243,6 @@ CREATE UNIQUE INDEX "WeeklyPlanExercise_weeklyPlanId_exerciseId_key" ON "WeeklyP
 CREATE INDEX "CalendarEntry_userId_date_idx" ON "CalendarEntry"("userId", "date");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "CalendarEntry_userId_date_key" ON "CalendarEntry"("userId", "date");
-
--- CreateIndex
 CREATE INDEX "Friendship_userId_idx" ON "Friendship"("userId");
 
 -- CreateIndex
@@ -187,19 +252,37 @@ CREATE INDEX "Friendship_friendId_idx" ON "Friendship"("friendId");
 CREATE UNIQUE INDEX "Friendship_userId_friendId_key" ON "Friendship"("userId", "friendId");
 
 -- AddForeignKey
+ALTER TABLE "VerificationToken" ADD CONSTRAINT "VerificationToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PasswordResetToken" ADD CONSTRAINT "PasswordResetToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "Exercise" ADD CONSTRAINT "Exercise_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "QuickStartExercise" ADD CONSTRAINT "QuickStartExercise_quickStartPlanId_fkey" FOREIGN KEY ("quickStartPlanId") REFERENCES "QuickStartPlan"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "QuickStartPlan" ADD CONSTRAINT "QuickStartPlan_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "QuickStartWeeklyPlan" ADD CONSTRAINT "QuickStartWeeklyPlan_quickStartPlanId_fkey" FOREIGN KEY ("quickStartPlanId") REFERENCES "QuickStartPlan"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "QuickStartExercise" ADD CONSTRAINT "QuickStartExercise_quickStartWeeklyPlanId_fkey" FOREIGN KEY ("quickStartWeeklyPlanId") REFERENCES "QuickStartWeeklyPlan"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "QuickStartExercise" ADD CONSTRAINT "QuickStartExercise_exerciseId_fkey" FOREIGN KEY ("exerciseId") REFERENCES "Exercise"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "WeeklyPlan" ADD CONSTRAINT "WeeklyPlan_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "QuickPlanUser" ADD CONSTRAINT "QuickPlanUser_quickStartPlanId_fkey" FOREIGN KEY ("quickStartPlanId") REFERENCES "QuickStartPlan"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "WeeklyPlan" ADD CONSTRAINT "WeeklyPlan_quickStartPlanId_fkey" FOREIGN KEY ("quickStartPlanId") REFERENCES "QuickStartPlan"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "QuickPlanUser" ADD CONSTRAINT "QuickPlanUser_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserPlan" ADD CONSTRAINT "UserPlan_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WeeklyPlan" ADD CONSTRAINT "WeeklyPlan_userPlanId_fkey" FOREIGN KEY ("userPlanId") REFERENCES "UserPlan"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "WeeklyPlanExercise" ADD CONSTRAINT "WeeklyPlanExercise_weeklyPlanId_fkey" FOREIGN KEY ("weeklyPlanId") REFERENCES "WeeklyPlan"("id") ON DELETE CASCADE ON UPDATE CASCADE;
