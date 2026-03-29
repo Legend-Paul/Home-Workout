516f07c3fc487428aac5e42ef2055edd4ebe95c7
Legend-Paul <qamunyamaina950@gmail.com>
2026-03-29 18:24:08 +0300

    Add update and delete button

--- frontend/src/pages/QuickPlan/QuickPlan.ts
+++ frontend/src/pages/QuickPlan/QuickPlan.ts
@@ -87,15 +87,33 @@ function renderPlansAndExercise(
             </svg>
             <p>Show quick plans</p>
         </div>
-        ${Button({
-          label: `
-                <svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6">
-                  </path>
-                </svg>
-                <p>Weekly Plan</p>`,
-          btnClass: styles["add-weekly-plan-btn"],
-          data: `data-plan-id="${planId}"`,
-        })}
+        <div class="${styles["plan-header-btns"]}">
+          ${Button({
+            label: `
+                  <svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6">
+                    </path>
+                  </svg>
+                  <p>Weekly Plan</p>`,
+            btnClass: styles["add-weekly-plan-btn"],
+            data: `data-plan-id="${planId}"`,
+          })}
+          <div class="${styles["plan-action-btns"]}">
+            ${Button({
+              label: `<svg fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"/></svg>`,
+              type: "button",
+              btnClass: styles["update-plan-btn"],
+              data: `data-plan-id="${planId}"`,
+            })}
+            ${Button({
+              label: `<svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+                        </svg>`,
+              type: "button",
+              btnClass: styles["delete-plan-btn"],
+              data: `data-plan-id="${planId}"`,
+            })}
+          </div>
+        </div>
       </div>
     ${renderQuickPlans(plans).outerHTML}
     ${renderQuickWeeklyPlans(weeklyPlans).outerHTML}
