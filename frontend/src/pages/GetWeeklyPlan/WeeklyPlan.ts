89c9d0078fe6aef9859d98d75adcd278a35079dd
Legend-Paul <qamunyamaina950@gmail.com>
2026-03-31 21:00:40 +0300

    Create weekly plan by id page

---
+++ frontend/src/pages/GetWeeklyPlan/WeeklyPlan.ts
@@ -0,0 +1,7 @@
+export default function WeeklyPlan() {
+  const mainApp = document.getElementById("app")!;
+  mainApp.innerHTML = `
+    <h1>Weekly Plan</h1>
+    <p>This is the Weekly Plan page. Here you can view and manage your weekly workout plans.</p>
+  `;
+}
