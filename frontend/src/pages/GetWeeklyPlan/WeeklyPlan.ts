7b07b2db2d8586952bc22f120ad3fd23666ce931
Legend-Paul <qamunyamaina950@gmail.com>
2026-04-02 21:07:30 +0300

    Create weekly exercise header

--- frontend/src/pages/GetWeeklyPlan/WeeklyPlan.ts
+++ frontend/src/pages/GetWeeklyPlan/WeeklyPlan.ts
@@ -1,5 +1,7 @@
+import Button from "../../components/Button/Button";
 import Notification from "../../components/Notification/Notification";
 import type { WeeklyPlan } from "../../utils/types";
+import styles from "./Weeklyplan.module.css";
 
 const backendUrl =
   import.meta.env.VITE_BACKEND_URL || import.meta.env.VITE_BACKEND_DEV_URL;
@@ -31,10 +33,32 @@ export default async function WeeklyPlan(params?: {
   const plan = await fetchWeeklyPlanById(planId, id);
   console.log("Fetched Weekly Plan:", plan);
   const mainApp = document.getElementById("main-app")!;
-  mainApp.innerHTML = `
-    <h1>Weekly Plan</h1>
-    <p>This is the Weekly Plan page. Here you can view and manage your weekly workout plans.</p>
-  `;
+
+  mainApp!.innerHTML = `
+      <div class="${styles["weekly-plan-container"]}">
+        <div class="${styles["weekly-plan-header"]}">
+          <div class="${styles["header-left"]}">
+            ${Button({
+              label: `<svg fill="none" stroke="currentColor" viewBox="0 0 24 24" width="18" height="18">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"/>
+                      </svg>
+                      Back`,
+              btnClass: styles["back-btn"],
+            })}
+            <h2 class="${styles["page-heading"]}">Weekly Plan</h2>
+          </div>
+          <div class="${styles["plan-header-btns"]}">
+            ${Button({
+              label: `<svg fill="none" stroke="currentColor" viewBox="0 0 24 24" width="18" height="18">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
+                      </svg>
+                      Add Exercise`,
+              btnClass: styles["add-weekly-plan-exercise-btn"],
+            })}
+          </div>
+        </div>
+      </div>`;
 }
 
 async function fetchWeeklyPlanById(
