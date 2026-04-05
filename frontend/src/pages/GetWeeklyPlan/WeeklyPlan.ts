d8e6344da747ddf92264dc1fc997abbfeced3406
Legend-Paul <qamunyamaina950@gmail.com>
2026-04-05 08:49:41 +0300

    Change add weekly exercise to edit weekly plan

--- frontend/src/pages/GetWeeklyPlan/WeeklyPlan.ts
+++ frontend/src/pages/GetWeeklyPlan/WeeklyPlan.ts
@@ -78,12 +78,11 @@ function renderWeeklyPlan(state: PageState) {
         </div>
         <div class="${styles["plan-header-btns"]}">
           ${Button({
-            label: `<svg fill="none" stroke="currentColor" viewBox="0 0 24 24" width="18" height="18">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
-                    </svg>
-                    Add Exercise`,
-            btnClass: styles["add-weekly-plan-exercise-btn"],
+            label: `<svg width="16" height="16" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
+            </svg> Edit Plan`,
+            btnClass: styles["edit-weekly-plan-exercise-btn"],
           })}
         </div>
 
